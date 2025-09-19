4ec7ba426081bd64327cd057876325931bfc82e6
denny-ddfe <101379619+denny-ddfe@users.noreply.github.com>
2025-09-19 11:35:25 +1000

    Create Ex1_3-1_5.jsx

---
+++ Part1/Ex1_3-1_5.jsx
@@ -0,0 +1,47 @@
+const Header = (props) => {
+  return (<h1>{props.course.name}</h1>)
+}
+
+const Content = (props) => {
+  return(
+    <>
+      {props.course.parts.map((item) => (
+      <p>{item.name} {item.exercises}</p>
+      ))}
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (<p>Number of exercises {props.course.parts.reduce((acc, el)=>acc+el.exercises,0)}</p>)
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+} 
+
+export default App
