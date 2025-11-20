aa45a0f881b403aa72409ba08bc3b224cfed373b
denny-ddfe <denny@dobbsdoherty.com.au>
2025-11-20 11:46:31 +1100

    Fix refresh issue for blogs

--- Part4/blogBackend/app.js
+++ Part4/blogBackend/app.js
@@ -32,6 +32,10 @@ if (process.env.NODE_ENV === 'test') {
   app.use('/api/testing', testingRouter) 
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
